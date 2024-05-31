d9426bb269c3cf371ff0e138e1a61170622e6d78
Your Name <you@example.com>
2024-05-31 01:25:26 +0000

    HUGE bug fix for laggy composer !

--- src/interfaces/coral_web/src/components/MessageContent.tsx
+++ src/interfaces/coral_web/src/components/MessageContent.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Transition } from '@headlessui/react';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState, useEffect } from 'react';
 
 import { CitationTextHighlighter } from '@/components/Citations/CitationTextHighlighter';
 import { DataTable } from '@/components/DataTable';
@@ -17,6 +17,7 @@ import {
   isErroredMessage,
   isFulfilledOrTypingMessage,
   isLoadingMessage,
+  BotState
 } from '@/types/message';
 import { cn } from '@/utils';
 
@@ -113,6 +114,10 @@ export const MessageContent: React.FC<Props> = ({ isLast, message, onRetry, over
       </>
     );
   } else {
+    // console.log("OVERRIDE", overrideText)
+    // (message.text === "") message.text : overrideText;
+    // console.log("NEWOVER", overrideText)
+    //message.annotations = {}
     const hasCitations =
       isTypingOrFulfilledMessage && message.citations && message.citations.length > 0;
     content = (
